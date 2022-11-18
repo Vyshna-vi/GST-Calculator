f5a492bdaa14743b7f735d07a3876c223a3164e1
Vyshnavi Santhosh <vyshnaviponnuzz0001@gmail.com>
2022-11-18 09:06:32 +0530

    Completed GST Calculator

--- gst.js
+++ gst.js
@@ -1,4 +1,10 @@
 const priceInput = document.querySelector("#price");
 const quantityInput = document.querySelector("#quantity");
-const button = document.querySelector("#result");
-b;
+const button = document.querySelector("#btn1");
+button.addEventListener("click", () => {
+  let price = parseFloat(priceInput.value);
+  let quantity = parseFloat(quantityInput.value);
+  console.log(priceInput, typeof price);
+  let GST = (price * quantity * 18) / 100 + price * quantity;
+  document.querySelector("p").innerHTML = GST;
+});
